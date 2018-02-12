85fa56b319808ec66c4e2a65c8136f9c7fd304f7
Alexander Zaitsev <zamazan4ik@tut.by>
2018-02-12 03:12:03 +0300

    Init

---
+++ prime_test.h
@@ -0,0 +1,54 @@
+/*Copyright (c) 2018 Alexander Zaitsev
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+        AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.*/
+
+#ifndef PROPOSAL_PRIME_TEST_PRIME_TEST_H
+#define PROPOSAL_PRIME_TEST_PRIME_TEST_H
+
+#include <cmath>
+
+namespace std
+{
+namespace experimental
+{
+
+template <typename T>
+bool is_prime(T num) noexcept
+{
+    T j=std::sqrt(static_cast<double>(n));
+    for(T i = 2; i <= j; ++i)
+    {
+        if(n % i == 0)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+template <typename T>
+bool is_probable_prime(T num) noexcept
+{
+    
+}
+
+}
+}
+
+#endif //PROPOSAL_PRIME_TEST_PRIME_TEST_H
